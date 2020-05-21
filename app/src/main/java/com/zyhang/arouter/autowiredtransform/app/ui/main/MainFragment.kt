b70795633aa86319d2b98b30847696930f0554c8
zyhang <izyhang@gmail.com>
2020-05-21 15:26:52 +0800

    remove MainFragment onCreate()

--- app/src/main/java/com/zyhang/arouter/autowiredtransform/app/ui/main/MainFragment.kt
+++ app/src/main/java/com/zyhang/arouter/autowiredtransform/app/ui/main/MainFragment.kt
@@ -1,13 +1,12 @@
 package com.zyhang.arouter.autowiredtransform.app.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.alibaba.android.arouter.facade.annotation.Autowired
-import com.alibaba.android.arouter.launcher.ARouter
 import com.zyhang.arouter.autowiredtransform.app.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -31,11 +30,6 @@ class MainFragment : Fragment() {
     var age = 0
 
     private lateinit var viewModel: MainViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-//        ARouter.getInstance().inject(this)
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
