77192e5d5005a9e925161a5434629fba7a61a00f
zyhang <izyhang@gmail.com>
2020-05-21 10:50:06 +0800

    implementation ARouter

--- app/src/main/java/com/zyhang/arouter/autowiredtransform/app/MainActivity.kt
+++ app/src/main/java/com/zyhang/arouter/autowiredtransform/app/MainActivity.kt
@@ -2,17 +2,19 @@ package com.zyhang.arouter.autowiredtransform.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.launcher.ARouter
 import com.zyhang.arouter.autowiredtransform.app.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        ARouter.init(application)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/com/zyhang/arouter/autowiredtransform/app/ui/main/MainFragment.kt
+++ app/src/main/java/com/zyhang/arouter/autowiredtransform/app/ui/main/MainFragment.kt
@@ -6,15 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alibaba.android.arouter.facade.annotation.Autowired
+import com.alibaba.android.arouter.launcher.ARouter
 import com.zyhang.arouter.autowiredtransform.app.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance() = MainFragment().apply {
+            arguments = Bundle().apply {
+                putString("name", "John")
+                putInt("age", 18)
+            }
+        }
     }
 
+    @JvmField
+    @Autowired
+    var name = "null"
+
+    @JvmField
+    @Autowired
+    var age = 0
+
     private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ARouter.getInstance().inject(this)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -27,5 +48,9 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        message.text = "name: $name" +
+                "\n" +
+                "age: $age"
     }
 }
